ad53e99b0e792fc47e7536edef30044bf3c7070f
gauravnegi123 <gauravnegi6783@gmail.com>
2025-08-14 13:51:56 +0530

    css update togle button js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle-password").onclick = () => {
+    const input = document.getElementById("password");
+    input.type = input.type === "password" ? "text" : "password";
+};
